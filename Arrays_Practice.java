b5bab4cccc9f8532d719639010e13f5208051f06
JusticeWolfLeo <115044383+JusticeWolfLeo@users.noreply.github.com>
2023-01-03 22:59:02 +0300

    Add files via upload

---
+++ Arrays_Practice.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class Arrays_Practice {
+    public static void main(String[] args) {
+        String[] seasons = {"Winter", "Spring", "Summer", "Autumn"}; //Времена года
+        String[] month = {"December", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November"}; // Месяцы
+        String[] week = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"}; //Дни недели
+        int[] days = {1, 2, 3, 4, 5, 6, 7, 8, 9}; // Числа месяца
+        System.out.println(seasons[0]);
+        System.out.println(Arrays.toString(month)); // полностью вывел месяцы в виде строки
+        System.out.println(week[2]);
+        System.out.println(days[3]);
+        int key = Arrays.binarySearch(days, 8); //попробовал поиск в массиве, число 8 на позиции 7
+        System.out.println(key);
+    }
+}
